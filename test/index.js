083d07882859f1e372ce52b3780ab03445e74937
Jannis R <mail@jannisr.de>
2016-09-09 01:01:49 +0200

    tests :white_check_mark:

--- test/index.js
+++ test/index.js
@@ -23,6 +23,7 @@ location.native(path.join(__dirname, 'success-mock')).catch(showError)
 	assert.strictEqual(loc.latitude,  23.456789)
 	assert.strictEqual(loc.longitude, 12.345678)
 	assert.strictEqual(loc.precision, 10)
+	assert.strictEqual(loc.native,    true)
 
 }).catch(showError)
 
@@ -31,5 +32,37 @@ location.native(path.join(__dirname, 'timeout-mock'))
 
 	// case: access denied/disabled or unable to locate
 	assert.strictEqual(err.code, 'ETIMEOUT')
+
+}).catch(showError)
+
+
+
+const resolveMock = () => new Promise((yay, nay) =>
+	setTimeout(() => yay({
+		latitude:  23.456789,
+		longitude: 12.345678,
+		precision: 15,
+		native:    false
+	}), 2000))
+
+location.nonNative(resolveMock).catch(showError)
+.then((loc) => {
+
+	// case: access granted, location successfully computed
+	assert.strictEqual(typeof loc, 'object')
+	assert.strictEqual(loc.latitude,  23.456789)
+	assert.strictEqual(loc.longitude, 12.345678)
+	assert.strictEqual(loc.precision, 10)
+
+}).catch(showError)
+
+const rejectMock = () => new Promise((yay, nay) =>
+	setTimeout(() => nay(new Error('timeout')), 2000))
+
+location.nonNative(rejectMock)
+.catch((err) => {
+
+	// case: access denied/disabled or unable to locate
+	assert.strictEqual(err.message, 'timeout')
 
 }).catch(showError)
