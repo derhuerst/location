e727463790b9107fa83495e5620cfdea0ae980ac
Jannis R <mail@jannisr.de>
2018-02-06 20:00:13 +0100

    move native call into separate file
    closes #6

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
 'use strict'
 
-const path    = require('path')
-const process = require('child_process')
 const triangulate = require('wifi-triangulate')
 
-const exe = path.join(__dirname, 'CoreLocationCLI')
-const args = [
-	  '-once', 'YES'
-	, '-format', '%latitude||%longitude||%h_accuracy'
-]
-
-const native = (timeout, locate, cb) => {
-	timeout = timeout || 10000
-	locate = locate || exe
-
-	process.execFile(locate, args, {timeout}, (err, out) => {
-		if (err) {
-			if (err.signal === 'SIGTERM') return cb(new Error('timeout'))
-			return cb(err)
-		}
-
-		out = out.split('||')
-		cb(null, {
-			  latitude:  parseFloat(out[0])
-			, longitude: parseFloat(out[1])
-			, precision: parseFloat(out[2])
-			, native:    true
-		})
-	})
-}
+const native = require('./native')
 
 const nonNative = (timeout, locate, cb) => {
 	timeout = timeout || 10000
